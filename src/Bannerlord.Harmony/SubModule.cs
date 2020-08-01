86cdeb2d7ee27521b566b156676a4dea4b32f78d
Vitaly Mikhailov <personal@aragas.org>
2020-08-01 18:31:52 +0300

    Init

---
+++ src/Bannerlord.Harmony/SubModule.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using TaleWorlds.MountAndBlade;
+
+namespace Bannerlord.SaveSystem
+{
+    public class SubModule : MBSubModuleBase { }
+}
