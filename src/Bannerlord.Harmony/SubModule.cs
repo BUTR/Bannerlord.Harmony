cb25b92f791b89bd74a02d7e8559ec2088a1d5e2
Vitaly Mikhailov <personal@aragas.org>
2020-08-16 22:54:46 +0300

    Assemblies does not work on 1.4.2
    Tags are no longer required

--- src/Bannerlord.Harmony/SubModule.cs
+++ src/Bannerlord.Harmony/SubModule.cs
@@ -2,5 +2,13 @@
 
 namespace Bannerlord.Harmony
 {
-    public class SubModule : MBSubModuleBase { }
+    public class SubModule : MBSubModuleBase
+    {
+        private readonly HarmonyLib.Harmony? _harmony;
+
+        public SubModule()
+        {
+            _harmony = null;
+        }
+    }
 }
