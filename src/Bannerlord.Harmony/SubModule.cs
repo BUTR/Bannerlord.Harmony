18bb064df7abc87daa60ce5706d6afe39cb33045
Vitaly Mikhailov <personal@aragas.org>
2020-08-21 00:24:28 +0300

    Restored msbuild files
    Restored <Assemblies/>
    Removed again direct 0Harmony usage

--- src/Bannerlord.Harmony/SubModule.cs
+++ src/Bannerlord.Harmony/SubModule.cs
@@ -2,13 +2,5 @@
 
 namespace Bannerlord.Harmony
 {
-    public class SubModule : MBSubModuleBase
-    {
-        private readonly HarmonyLib.Harmony? _harmony;
-
-        public SubModule()
-        {
-            _harmony = null;
-        }
-    }
+    public class SubModule : MBSubModuleBase { }
 }
