ff0fca06ed16258994534560a758673644d4c757
Aragas <personal@aragas.org>
2022-06-18 18:43:03 +0300

    Fix

--- src/Bannerlord.Harmony/Utils/InformationMessageUtils.cs
+++ src/Bannerlord.Harmony/Utils/InformationMessageUtils.cs
@@ -20,7 +20,7 @@ namespace Bannerlord.Harmony.Utils
             foreach (var constructorInfo in AccessTools.GetDeclaredConstructors(type, false) ?? Enumerable.Empty<ConstructorInfo>())
             {
                 var @params = constructorInfo.GetParameters();
-                if (@params.Length == 9)
+                if (@params.Length == 2 && @params[0].ParameterType == typeof(string) && @params[1].ParameterType == typeof(Color))
                 {
                     V1 = AccessTools2.GetDelegate<V1Delegate>(constructorInfo);
                 }
