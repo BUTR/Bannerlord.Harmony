f3e0f05056f23a46823397b5a515ac80484a2a90
Aragas <personal@aragas.org>
2022-09-17 15:27:35 +0300

    Better module resolution implementation

---
+++ src/Bannerlord.Harmony/Utils/HarmonyVersionAttribute.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Bannerlord.Harmony.Utils
+{
+    public class HarmonyVersionAttribute : Attribute
+    {
+        public Version Version { get; }
+
+        public HarmonyVersionAttribute(string version)
+        {
+            Version = Version.Parse(version);
+        }
+    }
+}
